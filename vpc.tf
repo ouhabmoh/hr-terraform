fbc0cab8884d0d7c2b7eaaec9618543a7d670af1
ouhab <mohamed-moh98@live.com>
2024-05-14 23:47:49 +0100

    fix ecr policy

--- vpc.tf
+++ vpc.tf
@@ -15,10 +15,12 @@ module "vpc" {
   single_nat_gateway   = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = 1
   }
 }
